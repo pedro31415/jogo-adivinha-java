6a6fb919a1535cc76add065ada990bacba7c0e8f
Pedro <pedrogoku80@gmail.com>
2023-07-26 17:02:25 -0300

    Jogo de adivinhação

---
+++ src/App.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int op,num;
+      int count = 0;
+      Scanner scan = new Scanner(System.in);
+      Random random = new Random();
+      int valorR = random.nextInt(11);
+      do {
+        System.out.println("1 - Jogar");
+        System.out.println("2 - Sair");
+        System.out.println("Opção desejada: ");
+        op = scan.nextInt();
+        switch (op){
+          case 1:
+             do {
+             System.out.println("Digite um número de 0 a 10: ");
+             num = scan.nextInt();
+             if(num>10 || num<0){
+              System.out.println("Opção inválida");
+              count--;
+             } else {
+              if(num == valorR){
+              System.out.format("Parabéns, você venceu o jogo na %d tentativa%n", count);
+              System.out.println("Essa partida foi finalizada");
+              Thread.sleep(1000);
+              break;
+             } else {
+              System.out.println("Você errou, tente novamente ");
+              }
+            }  
+             count++;
+             } while(num != valorR);
+            break;
+          case 2:
+          System.out.println("saindo...");
+          Thread.sleep(1000);
+          break;
+          default: 
+          System.out.println("Opção inválida");
+          Thread.sleep(1000);
+          break;
+        }
+      } while(op != 2);
+      scan.close();
+      System.out.println("Jogo Finalizado");
+    }
+}
